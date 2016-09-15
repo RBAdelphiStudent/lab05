0e552f183786967ffd610ae786db4c0da7455015
RYAN BARRETT <ryanbarrett@adelphi.internal>
2016-09-15 11:51:49 -0400

    Function contract added for most methods.

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -13,18 +13,20 @@ public class PersonCreator
     private String sex;
     private int height;
     private int weight;
-    private int socialSecurityNumber1;
-    private int socialSecurityNumber2;
-    private int socialSecurityNumber3;
+    private String socialSecurityNumber;
     private String address;
-    private int areaCode;
-    private int prefix;
-    private int last;
-    private int birthMonth;
-    private int birthYear;
-    private int birthDay;
+    private String phoneNumber;
+   private String birthday;
     /**
      * Creates a person and gives them a name and surname.
+     * The method's input consists of two Strings, a name and a surname.
+     * The method constructs a new PersonCreator class (a person)
+     * and initializes their first and last names (firstName and lastName
+     * based on the input.
+     * Example: The user enters these Strings
+     * name=David
+     * surname=Terrell
+     * The class then sets firstName to equal "Dave" and lastName to equal "Terrell".
      */
     public PersonCreator(String name, String surname){
         firstName=name;
@@ -32,88 +34,126 @@ public class PersonCreator
     }
     /**
      * Assigns a person an age in years entered by the user.
+     * The function's input consists of a single integer (yearsOld).
+     * The method changes the person's age to match this number.
+     * Example: If the user enters 22 and assigns it to yearsOld, the age (field value)
+     * of the person being created  will be changed to 22 (age).
      */
     public void assignAge(int yearsOld){
         age=yearsOld;
     }
     /**
      * Assigns the person the male sex.
+     * This method has no user input.
+     * This method is meant to change the PersonCreator object's String sex to "Male".
+     * Example: The user initializes the method; String sex is changed to "Male".
      */
     public void assignMale(){
         sex="Male";
     }
     /**
      * Assigns the person the female sex.
+     * This method has no user input.
+     * This method is meant to change the PersonCreator object's String sex to "Female".
+     * Example: The user initializes the method; String sex is changed to "Female".
      */
     public void assignFemale(){
         sex="Female";
     }
     /**
      * Assigns the person a height in inches entered by the user.
+     * In this method, the user enters a height in inches (int heightInInches).
+     * The method sets the PersonCreator object's height to heightInInches.
+     * Example: If the user enters 65, the PersonCreator object's height will be set to 65.
      */
     public void assignHeight(int heightInInches){
         height=heightInInches;
     }
     /**
      * Assigns the person a weight in pounds entered by the user.
+     * In this method, the user enters a weight in pounds (int weightInPounds).
+     * The method sets the PersonCreator object's weight to weightInPounds.
+     * Example: If the user enters 142, the PersonCreator object's weight will be set to 142.
      */
     public void assignWeight(int weightInPounds){
         weight=weightInPounds;
     }
     /**
      * Assigns the person a SSN entered by the user if the SSN is valid.
+     * The method has 3 ints as inputs, each representing a different part of the SSN.
+     * The method checks if these ints are valid, and then combines them to create
+     * the value of String socialSecurityNumber. If any of the values are invalid, the method
+     * prints an error message.
+     * Example: If the user entered 142, 287, and 6792 as the three input ints, the String
+     * socialSecurityNumber would be set to "142-287-6792". If the last value was 99999 instead,
+     * the system would produce an error message.
      */
     public void assignSSN(int ssn1, int ssn2, int ssn3){
         if(100 <=ssn1 && ssn1<=999 && 10<=ssn2 && ssn2<=99 && 1000<=ssn3 && ssn3<=9999){
-            socialSecurityNumber1=ssn1;
-            socialSecurityNumber2=ssn2;
-            socialSecurityNumber3=ssn3;
+            socialSecurityNumber=ssn1+"-"+ssn2+"-"+ssn3;
         }
         else{
-            System.err.println("That is not a valid social security number.");
+            System.err.println("Please enter a valid social security number.");
         }
     }
     /**
      * Assigns the person an address entered by the user.
+     * The user inputs String personAddress.
+     * The method sets the PersonCreator's String address to personAddress.
+     * Example: If the user enters "42 Godot Street" for personAddress, the PersonCreator object's
+     * String address will be set to "42 Godot Street".
      */
     public void assignAddress(String personAddress){
         address=personAddress;
     }
     /**
      * Assigns the person a phone number entered by the user if the phone number is valid.
+     * The method has 3 ints as inputs, each representing a different part of the phoneNumber.
+     * The method checks if these ints are valid, and then combines them to create
+     * the value of String phoneNumber. If any of the values are invalid, the method
+     * prints an error message instead.
+     * Example: If the user entered 516, 472, and 8447 as the three input ints, the String
+     * socialSecurityNumber would be set to "516-472-8447". If the last value was 99999 instead,
+     * the system would produce an error message.
      */
     public void assignPhoneNumber(int codeOfArea, int numberPrefix, int lastPart){
         if(100<=codeOfArea && codeOfArea<=999 && 100<=numberPrefix && numberPrefix<=999 && 1000<=lastPart &&lastPart<=9999){
-            areaCode=codeOfArea;
-            prefix=numberPrefix;
-            last=lastPart;
+            phoneNumber=codeOfArea+"-"+numberPrefix+"-"+lastPart;
         }
         else{
-            System.err.println("That is not a valid phone number.");
+            System.err.println("Please enter a valid phone number.");
         }
     }
     /**
      * Assigns the person a birthday, birth month, and birth year entered by the user if they are all valid.
+     * The user inputs three ints, monthMM, dayDD, and yearYYYY, which are all meant to represent components
+     * of a birthday.
+     * The method combines these ints to set the value of the PersonCreator object's String birthday.
+     * Example: If the user entered 4, 6, and 1992 as monthMM, dayDD, and yearYYYY, the method would set String
+     * birthday to "4/6/1992".
      */
     public void assignBirthday(int monthMM, int dayDD, int yearYYYY){
         if(1<=monthMM && monthMM<=12 && 1<=dayDD && dayDD<=31 && 1900<=yearYYYY && yearYYYY<=2016){
-            birthMonth=monthMM;
-            birthDay=dayDD;
-            birthYear=yearYYYY;
+            birthday=monthMM+"/"+dayDD+"/"+yearYYYY;
         }
         else{
-            System.err.println("That is not a valid birthday.");
+            System.err.println("Please enter a valid birthday.");
         }
     }
+    /**
+     * Prints the PersonCreator object's field values as the person's information.
+     * This method has no inputs.
+     * The method is meant to list all of the PersonCreator object's field values.
+     */
     public void printPersonInformation(){
         System.out.println("Name: "+firstName+" "+lastName);
         System.out.println("Age: "+age+ " years old");
         System.out.println("Sex: "+sex);
         System.out.println("Height: "+height+" inches");
         System.out.println("Weight: "+weight+" pounds");
-        System.out.println("SSN: "+socialSecurityNumber1+"-"+socialSecurityNumber2+"-"+socialSecurityNumber3);
+        System.out.println("SSN: "+socialSecurityNumber);
         System.out.println("Address: "+address);
-        System.out.println("Phone number: "+areaCode+"-"+prefix+"-"+last);
-        System.out.println("Date of Birth: "+birthMonth+"/"+birthDay+"/"+birthYear);
+        System.out.println("Phone number: "+phoneNumber);
+        System.out.println("Date of Birth: "+birthday);
     }
 }
